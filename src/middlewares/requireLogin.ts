9908c2c95ec421b318d3e3ff3befac41ed4386e1
kg-kartik <goel.kartik39@gmail.com>
2022-02-21 16:54:51 +0530

    Added utils

---
+++ src/middlewares/requireLogin.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import Users from "../models/UserModel";
+import {Response,NextFunction} from "express"
+import RequestWithUser from "src/types/RequestWithUser";
+
+require("dotenv").config();
+
+const secret = process.env.JWT_SECRET;
+
+interface JwtPayload{
+    _id:String;
+}
+
+export const requireLogin =  async (req:RequestWithUser, res:Response, next:NextFunction) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.status(401).json({
+            error: "no headers provided",
+        });
+    }
+
+    //Get the token from Bearer "token"
+    const token = authorization.replace("Bearer ", "");
+
+    //Verifying the user token for accessing protected pages
+    //which user gets after successful login
+    
+    const {_id} = jwt.verify(token,secret) as JwtPayload;
+
+    req.user = await Users.findById({
+        _id
+    })
+  
+    next();
+    
+};
